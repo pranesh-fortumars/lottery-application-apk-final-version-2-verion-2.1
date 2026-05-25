d06a8e4370ed16424a8423eba35c44a8403d07a6
pranesh-fortumars <pranesh@fortumars.com>
2026-05-25 09:49:38 +0530

    chore: bump version to 1.0.7 (v8) for production APK release

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const APP_VERSION = "v1.0.0";
-export const BUILD_VERSION = "v1.0.0 Production Release";
+export const APP_VERSION = "v1.0.7";
+export const BUILD_VERSION = "v1.0.7 Production Release (v8)";
