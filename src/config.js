ee0050040df54834d6460b1e46295b1bc64e0437
pranesh-fortumars <pranesh@fortumars.com>
2026-05-26 11:18:31 +0530

    chore(release): bump application version to v1.0.9

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const APP_VERSION = "v1.0.8";
-export const BUILD_VERSION = "v1.0.8 Production Release (v9)";
+export const APP_VERSION = "v1.0.9";
+export const BUILD_VERSION = "v1.0.9 Production Release (v10)";
